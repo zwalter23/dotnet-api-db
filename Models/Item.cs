71dfc695d3046c2bcfc40b05b581c4239b43b29e
zwalter23 <zimmerwalter@gmail.com>
2021-10-26 11:04:11 +0200

    Initial commit

---
+++ Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_core_with_api_and_database.Models
+{
+    public class Item
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Models/ItemContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_core_with_api_and_database.Models
+{
+    public class ItemContext:DbContext
+    {
+        public ItemContext(DbContextOptions<ItemContext> options):base(options)
+        {
+        }
+        public DbSet<Item> Items { get; set; }
+    }
+}
