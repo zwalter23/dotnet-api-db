486fb34c51554f629b21a9ddb4ca44fcc963a25f
zwalter23 <zimmerwalter@gmail.com>
2021-10-26 17:05:21 +0200

    Add create function

---
+++ Controllers/ItemsMVController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using dotnet_core_with_api_and_database.Models;
+
+namespace dotnet_core_with_api_and_database.Controllers
+{
+    [Route("ItemsMV")]
+    [ApiController]
+    public class ItemsMVController : Controller
+    {
+        private readonly ItemContext _context;
+
+        public ItemsMVController(ItemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ItemsMV
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Items.ToListAsync());
+        }
+
+        // GET: ItemsMV/Details/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // GET: ItemsMV/Create
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ItemsMV/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create([FromForm] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                Console.WriteLine(item.Name);
+                _context.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+
+        // GET: ItemsMV/Edit/5
+        [HttpGet("Edit/{id}")]
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+
+        // POST: ItemsMV/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost("Edit/{id}")]
+
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Name")] Item item)
+        {
+            if (id != item.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(item);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ItemExists(item.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+
+        // GET: ItemsMV/Delete/5
+        [HttpGet("Delete/{id}")]
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // POST: ItemsMV/Delete/5
+        [HttpPost("Delete/{id}")]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            _context.Items.Remove(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ItemExists(long id)
+        {
+            return _context.Items.Any(e => e.Id == id);
+        }
+    }
+}
